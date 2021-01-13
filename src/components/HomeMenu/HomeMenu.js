d7b2fdc8628b849f0c7ecf2702b5f05b27e0e2af
Adrien Barbier <abarbier@debutant-e.pro>
2021-01-13 22:57:08 +0100

    Adding gsap animation on homepage

--- src/components/HomeMenu/HomeMenu.js
+++ src/components/HomeMenu/HomeMenu.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import classes from './HomeMenu.module.css'
 import { Link } from "gatsby";
+import { gsap } from "gsap/all";
 
 const HomeMenu = (props) => {
+    let name = useRef(null)
+    let menu = useRef(null)
+    let close = useRef(null)
+
+    const timeline = gsap.timeline();
+
+    const clickHandler = () => {
+        timeline.to(name.current, {duration: 0.4, opacity: 0, ease: "power2.inOut" });
+        timeline.to(name.current, {duration: 0, css: {visibility: 'hidden'}})
+        timeline.to(menu.current, {duration: 0, css: {visibility: 'visible'}})
+        timeline.to(menu.current, {duration: 0.4, opacity: 1, ease: "power2.inOut" });
+        timeline.to(close.current, {duration: 0, css: {visibility: 'visible'}, delay: -0.4})
+        timeline.to(close.current, {duration: 0.4, opacity: 1, ease: "power2.inOut", delay: -0.4 });
+    }
+
+    const closeHandler = () => {
+        timeline.to(close.current, {duration: 0.4, opacity: 0, ease: "power2.inOut", delay: -0.2 });
+        timeline.to(close.current, {duration: 0, css: {visibility: 'hidden'}, delay: -0.2})
+        timeline.to(menu.current, {duration: 0.4, opacity: 0, ease: "power2.inOut" });
+        timeline.to(menu.current, {duration: 0, css: {visibility: 'hidden'}})
+        timeline.to(name.current, {duration: 0, css: {visibility: 'visible'}})
+        timeline.to(name.current, {duration: 0.4, opacity: 1, ease: "power2.inOut" });
+    }
+
     return (
-        <div className={`${classes.homeMenuWrapper} ${props.show ? classes.displayMenu : ''}`}>
-            <ul className={classes.navLinksContainer}>
-                <li className={classes.underlineContainer}>
-                    <Link className={classes.underline} to="/work">WORK</Link>
-                </li>
-                <li className={classes.underlineContainer}>
-                    <Link className={classes.underline} to="/about">ABOUT</Link>
-                </li>
-                <li className={classes.underlineContainer}>
-                    <Link className={classes.underline} to="/contact">CONTACT</Link>
-                </li>
-            </ul>
+        <div>
+            <div ref={name} onClick={() => clickHandler()} className={`${classes.name}`}>
+                Aurore Demierre
+            </div>
+            <div ref={menu} className={`${classes.homeMenuWrapper}`}>
+                <ul className={classes.navLinksContainer}>
+                    <li className={classes.underlineContainer}>
+                        <Link className={classes.underline} to="/work">WORK</Link>
+                    </li>
+                    <li className={classes.underlineContainer}>
+                        <Link className={classes.underline} to="/about">ABOUT</Link>
+                    </li>
+                    <li className={classes.underlineContainer}>
+                        <Link className={classes.underline} to="/contact">CONTACT</Link>
+                    </li>
+                </ul>
+            </div>
+            <div ref={close} onClick={() => closeHandler()} className={classes.close}>Close</div>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,13 @@ import HomeMenu from '../components/HomeMenu/HomeMenu'
 const IndexPage = () => {
   const [showMenu, setShowMenu] = useState(false)
 
-  let app = useRef(null);
-  let picture = useRef(null);
+  let menu = useRef(null);
+  let app = useRef(null)
 
   const timeline = gsap.timeline();
 
   useEffect(() => {
-    timeline.to(app.current, {duration: 0.6, css: {visibility: 'visible'}})
-    timeline.from(picture.current, {duration: 0.6, opacity: 0, ease: "power2.inOut" });
+    timeline.to(app.current, {duration: 0.2, css: {visibility: 'visible'}})
   }, [])
 
   const clickHandler = () => {
@@ -30,11 +29,8 @@ const IndexPage = () => {
   return (
     <div ref={app} className={classes.app}>
       <Layout>
-        <div onClick={() => clickHandler()} className={`${classes.name} ${showMenu ? classes.hide : ''}`}>
-          Aurore Demierre
-        </div>
+
         <HomeMenu show={showMenu}/>
-        <div onClick={() => setShowMenu(!showMenu)} className={`${classes.close} ${showMenu ? classes.display : ''}`}>Close</div>
         <video autoPlay muted loop className={classes.video}>
             <source src={video} type='video/mp4' />
         </video>
